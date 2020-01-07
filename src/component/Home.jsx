3ec5757d9f6b1820f873469947e29c264d52899d
abdelwali bladi <abdelwalibladi@carboatmedia.fr>
2020-01-07 15:41:18 +0100

    [home page]

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import styles from "./Home.module.scss";
 
 function Home() {
   const companies = useSelector(state => state.companies);
-  return companies.map(company => (
-    <div key={company.id}>
-      <Link to={`/company/${company.id}`}>{company.name} </Link>
+  return (
+    <div className={styles.companyContainer}>
+      {companies.map(company => (
+        <Link to={`/company/${company.id}`} className={styles.link}>
+          <div className={styles.companyCard}>
+            <img src={company.logo} className={styles.companyCardThumbnail} />
+            <div key={company.id} className={styles.info}>
+              <h1 className={styles.companyCardTitle}>{company.name}</h1>
+            </div>
+          </div>
+        </Link>
+      ))}
     </div>
-  ));
+  );
 }
 
 export default Home;
